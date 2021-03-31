4bbb90f81a891812d4b7c32393f723c83bb26fb3
Andres Made <andres.made.dev@gmail.com>
2021-03-30 22:58:01 -0400

    Initial commit

---
+++ Github test/Github_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_testApp.swift
+//  Github test
+//
+//  Created by Andres Made on 3/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Github_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
